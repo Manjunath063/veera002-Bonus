6011481759d8a86816c9b8312f0fdb365976ced4
Manjunath063 <manjunathjangam198@gmail.com>
2025-07-11 10:31:06 +0530

    commit

--- Backend/server.js
+++ Backend/server.js
@@ -103,6 +103,6 @@ async function initializeDatabase() {
 // Start server
 initializeDatabase().then(() => {
   app.listen(port, () => {
-    console.log(`Server running at http://13.49.224.11:${port}`);
+    console.log(`Server running at http://13.60.10.12:${port}`);
   });
 });
